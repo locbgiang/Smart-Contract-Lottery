afc3e4732e43c3e45940f96f9eb50543c91d492a
locgiang <locbgiang@gmail.com>
2024-12-03 23:40:32 -0800

    raffle start

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,5 +1,41 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.18;
 
-contract Raffle {}
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+import {console} from "forge-std/console.sol";
+
+/**
+ * @title A Raffle contract
+ * @author Loc Giang
+ * @notice This contract is for creating a sample raffle
+ * @dev Implement Chainlink VRF2.5
+ */
+contract Raffle {
+    // 1. enter raffle
+    // 2. check upkeep
+    // 3. perform upkeep
+    // 4. fulfill random words
+}
