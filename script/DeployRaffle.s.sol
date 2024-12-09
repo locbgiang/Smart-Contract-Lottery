9d13faeb726a485034645bb4523e122da0d55700
locgiang <locbgiang@gmail.com>
2024-12-08 16:35:39 -0800

    deploy script finish

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.18;
 import {Script} from "forge-std/Script.sol";
 import {Raffle} from "../src/Raffle.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
+import {CreateSubscription, FundSubscription, AddConsumer} from "./Interactions.s.sol";
 
 contract DeployRaffle is Script {
     // automatically run this function when script is called
@@ -15,8 +16,43 @@ contract DeployRaffle is Script {
         // call getConfig function in HelperConfig.s.sol to determind the configuration
         HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
 
+        // if there are no current VRF subscription, we create it
         if (config.subscriptionId == 0) {
             // create subscription
+            CreateSubscription createSubscription = new CreateSubscription();
+            (config.subscriptionId, config.vrfCoordinator) = createSubscription
+                .createSubscription(config.vrfCoordinator, config.account);
+
+            // fund it
+            FundSubscription fundSubscription = new FundSubscription();
+            fundSubscription.fundSubscription(
+                config.vrfCoordinator,
+                config.subscriptionId,
+                config.link,
+                config.account
+            );
         }
+
+        // deploy raffle contract with given environment configuration
+        vm.startBroadcast(config.account);
+        Raffle raffle = new Raffle(
+            config.entranceFee,
+            config.interval,
+            config.vrfCoordinator,
+            config.gasLane,
+            config.subscriptionId,
+            config.callbackGasLimit
+        );
+        vm.stopBroadcast();
+
+        AddConsumer addConsumer = new AddConsumer();
+        addConsumer.addConsumer(
+            address(raffle),
+            config.vrfCoordinator,
+            config.subscriptionId,
+            config.account
+        );
+
+        return (raffle, helperConfig);
     }
 }
